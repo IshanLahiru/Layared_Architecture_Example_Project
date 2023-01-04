8481e91ee3e06a0e7895a435f990186e883b4f58
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-01-04 13:09:10 +0530

    Initial commit

---
+++ src/main/java/Appinitializer.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Appinitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL resource = this.getClass().getResource("view/MainForme.fxml");
+        primaryStage.setScene(new Scene(FXMLLoader.load(resource)));
+        primaryStage.show();
+    }
+}
